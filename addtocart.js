f3fc8c2c252c031cd76af620a70b9a063a22e64b
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2023-11-29 21:42:29 +0530

    Add files via upload

--- addtocart.js
+++ addtocart.js
@@ -1,19 +1,14 @@
-// addtocart.js
-
 // Check if the cartItems array is stored in localStorage, and retrieve it
 var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 
 // Function to add an item to the cart
 function addtocart(item) {
-    // Get the details of the selected item
-    var itemName = document.getElementById(item).querySelector("h1").innerText;
-    var itemPrice = parseFloat(document.getElementById(item).querySelector("h4").innerText.replace('$', ''));
+    var itemName = document.getElementById(item).querySelector(".cart-info p").innerText;
+    var itemPrice = parseFloat(document.getElementById(item).querySelector(".cart-info small").innerText.replace('$', ''));
     var itemQuantity = parseInt(document.getElementById(item).querySelector("input").value);
 
-    // Calculate subtotal for the item
     var itemSubtotal = itemPrice * itemQuantity;
 
-    // Create an object representing the item
     var cartItem = {
         name: itemName,
         price: itemPrice,
@@ -21,20 +16,47 @@ function addtocart(item) {
         subtotal: itemSubtotal
     };
 
-    // Add the item to the cart
     cartItems.push(cartItem);
 
-    // Save the updated cartItems array to localStorage
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
 
-    // Optionally, you can show a confirmation message to the user
+    updateCartDisplay();
+
     alert(itemName + " added to cart!");
 }
 
 // Function to update the cart display
 function updateCartDisplay() {
-    // You need to implement this function based on your cart page structure
-    // For example, update the table rows with cartItems data
-    // Update the subtotal, tax, and total values
-    // This function will depend on the HTML structure of your cart page
+    var cartTable = document.querySelector('.cart-page table');
+    var totalTable = document.querySelector('.total-price table');
+
+    if (cartItems.length > 0) {
+        cartTable.innerHTML = '';
+        var tbody = document.createElement('tbody');
+
+        cartItems.forEach(function (item) {
+            var row = document.createElement('tr');
+            row.innerHTML = '<td>' + item.name + '</td><td>' + item.quantity + '</td><td>$' + item.subtotal.toFixed(2) + '</td>';
+            tbody.appendChild(row);
+        });
+
+        cartTable.appendChild(tbody);
+
+        var subtotal = cartItems.reduce(function (total, item) {
+            return total + item.subtotal;
+        }, 0);
+
+        var tax = 0.1 * subtotal; // Assuming 10% tax
+        var total = subtotal + tax;
+
+        totalTable.innerHTML = '<tr><td>Subtotal</td><td>$' + subtotal.toFixed(2) + '</td></tr>' +
+                              '<tr><td>Tax</td><td>$' + tax.toFixed(2) + '</td></tr>' +
+                              '<tr><td>Total</td><td>$' + total.toFixed(2) + '</td></tr>';
+    } else {
+        cartTable.innerHTML = '<tr><td colspan="3">Your cart is empty.</td></tr>';
+        totalTable.innerHTML = ''; // Clear total values if cart is empty
+    }
 }
+
+// Call the updateCartDisplay function to initially populate the cart on page load
+updateCartDisplay();
